5ceb5fa5dbce52ae41d73536766f679e17c512ad
localband <agvrideteam@gmail.com>
2022-06-27 23:57:46 +0300

    Social Network Project v. 1.0
    Added the ability to change the user's photo.
    
    Fixed a bug with displaying your own profile page after clicking on the Profile page link.

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ProfileInfo from "./ProfileInfo/ProfileInfo";
 import MyPostsContainer from "./MyPosts/MyPostsContainer";
+import {savePhoto} from "../../redux/profile-reducer";
 
 
 const Profile = (props) => {
 
     return (
         <div>
-            <ProfileInfo profile={props.profile} status={props.status} updateStatus={props.updateStatus}/>
+            <ProfileInfo isOwner={props.isOwner} profile={props.profile}
+                         status={props.status} updateStatus={props.updateStatus}
+                         savePhoto={props.savePhoto}/>
             <MyPostsContainer />
         </div>
     )
--- src/components/Profile/ProfileInfo/ProfileInfo.jsx
+++ src/components/Profile/ProfileInfo/ProfileInfo.jsx
@@ -3,11 +3,17 @@ import s from './ProfileInfo.module.css';
 import Preloader from "../../common/preloader/preloader";
 import ProfileStatus from "./ProfileStatus";
 import ProfileStatusWithHooks from "./ProfileStatusWithHooks";
+import userPhoto from "../../../assets/images/user.png"
 
-
-const ProfileInfo = ({profile, updateStatus, status}) => {
+const ProfileInfo = ({profile, updateStatus, status, isOwner, savePhoto}) => {
     if (!profile) {
         return <Preloader />
+    }
+
+    const onMainPhotoSelected = (e) => {
+        if (e.target.files.length) {
+            savePhoto (e.target.files[0]);
+        }
     }
     return (
         <div>
@@ -15,7 +21,8 @@ const ProfileInfo = ({profile, updateStatus, status}) => {
                 <img src='https://images.pexels.com/photos/248797/pexels-photo-248797.jpeg?auto=compress&cs=tinysrgb&h=350'/>
             </div>
             <div className={s.descriptionBlock}>
-                <img src={profile.photos.large}/>
+                <img src={profile.photos.large || userPhoto} className={s.mainPhoto}/>
+                <div> { isOwner && <input type={"file"} onChange={onMainPhotoSelected}/>} </div>
                 <ProfileStatusWithHooks status={status} updateStatus={updateStatus}/>
             </div>
         </div>
