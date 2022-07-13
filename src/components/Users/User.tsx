3742a2b5f98f67c2712d6f82d43a3467e33d47a1
localband <agvrideteam@gmail.com>
2022-07-14 00:06:30 +0300

    Social Network Project v. 1.1
    User.tsx component has been overwritten.
    
    tsconfig.json has been added.(Problem has been solved)
    declare module "*.png" added to declaration.d.ts

--- src/components/Users/User.tsx
+++ src/components/Users/User.tsx
@@ -2,9 +2,16 @@ import React from "react";
 import s from "./User.module.css";
 import userPhoto from "../../assets/images/user.png";
 import {NavLink} from "react-router-dom";
+import {PhotoType, UserType} from "../../types/types";
 
-
-let User = ({user, followingInProgress, unfollow, follow}) => {
+type PropsType = {
+    user: UserType
+    followingInProgress: Array<number>
+    photos: PhotoType
+    unfollow: (userId: number) => void
+    follow: (userId: number) => void
+}
+let User: React.FC<PropsType> = ({user, followingInProgress, unfollow, follow}) => {
     return (
         <div>
             <span>
